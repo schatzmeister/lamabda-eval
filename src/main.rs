d683f71e54230ebdeac3770a82acabe027b1d7cc
schatzmeister <graf.schatzmeister@gmail.com>
2020-02-23 22:20:36 +0100

    Added Tokenizer. Does not work yet.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::io::{stdin, stdout, Write};
 use std::process::exit;
 
+#[derive(Clone, Debug, Eq, PartialEq)]
+enum Token {
+    Lambda,
+    Param(String),
+    Dot,
+    Body(String),
+    Command(String),
+}
+
 fn startup() {
     println!("Welcome to Λeval v{}", env!("CARGO_PKG_VERSION"));
 }
@@ -19,11 +28,54 @@ fn read(mut input: &mut String) {
     }
 }
 
-fn eval(input: &mut String) -> String {
-    if input == ":quit" {
+fn tokenize(s: String) -> Vec<Token> {
+    let mut toks = Vec::new();
+    let mut sit = s.chars().peekable();
+    
+    loop {
+        let ch = sit.next();
+        if ch == None {
+            break;
+        }
+        let ch = ch.unwrap();
+        
+        if ch == ':' {
+            return vec![Token::Command(s[1..].to_string())];
+        }
+        
+        if ch == 'λ' || ch == '\\' {
+            toks.push(Token::Lambda)
+        } else if ch == '.' {
+            toks.push(Token::Dot)
+        } else {
+            let mut cont = String::new();
+            cont.push(ch);
+            while let Some(x) = sit.peek() {
+                match x {
+                    '.' | 'λ' | '\\' => break,
+                    _ => {
+                        cont.push(*x);
+                        sit.next();
+                    },
+                }
+            }
+            match sit.next() {
+                None => (),
+                Some('.') => toks.push(Token::Param(cont)),
+                _ => toks.push(Token::Body(cont)),
+            }
+        }
+    }
+    toks
+}
+
+fn eval(input: Vec<Token>) -> String {
+    if input.is_empty() {
+        return String::new()
+    } else if input[0] == Token::Command("quit".to_string()) {
         exit(0);
     }
-    input.clone()
+    format!("{:?}", input)
 }
 
 fn main() {
@@ -33,6 +85,6 @@ fn main() {
         print!("> ");
         let mut input = String::new();
         read(&mut input);
-        println!("{}", eval(&mut input))
+        println!("{}", eval(tokenize(input)));
     }
 }
