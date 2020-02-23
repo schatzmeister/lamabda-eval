fd31d96b6c90c699dfe47b5f918af21bd2461457
schatzmeister <graf.schatzmeister@gmail.com>
2020-02-23 13:51:29 +0100

    Simple REPL, can only exit.

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::io::{stdin, stdout, Write};
+use std::process::exit;
+
+fn startup() {
+    println!("Welcome to Λeval v{}", env!("CARGO_PKG_VERSION"));
+}
+
+fn read(mut input: &mut String) {
+    let _ = stdout().flush();
+    match stdin().read_line(&mut input) {
+        Ok(_) => (),
+        Err(error) => println!("Couldn’t handle input: {}", error),
+    }
+    if let Some('\n') = input.chars().next_back() {
+        input.pop();
+    }
+    if let Some('\r') = input.chars().next_back() {
+        input.pop();
+    }
+}
+
+fn eval(input: &mut String) -> String {
+    if input == ":quit" {
+        exit(0);
+    }
+    todo!()
+}
+
+fn main() {
+    let mut input = String::new();
+    
+    startup();
+    
+    loop {
+        print!("> ");
+        read(&mut input);
+        println!("{}", eval(&mut input))
+    }
+}
